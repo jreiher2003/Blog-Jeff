597a3e88946863ec1e33b122e9f077e7b6595193
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-25 09:43:03 -0500

    Add submit button to login form

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField, TextAreaField, BooleanField
+from wtforms import TextField, PasswordField, TextAreaField, BooleanField, SubmitField
 from wtforms.validators import DataRequired,Length
 
 class LoginForm(Form):
 	username = TextField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember me.')
+	submit = SubmitField("Sign In")
 
 
 class MessageForm(Form):
