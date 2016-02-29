4abb33d1ebcce572f3eda8db7f83ae387a7f2b90
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-29 11:56:32 -0500

    Add RegisterForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired,Length
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(Form):
 	username = TextField('Username', validators=[DataRequired()])
@@ -13,3 +13,10 @@ class MessageForm(Form):
 	title = TextField('Title', validators=[DataRequired()])
 	description = TextAreaField('Description', validators=[DataRequired(),Length(max=140)])
 	submit = SubmitField("Create Post")
+
+
+class RegisterForm(Form):
+    username = TextField('username', validators=[DataRequired(), Length(min=3, max=25)])
+    email = TextField('email', validators=[DataRequired(), Email(message=None), Length(min=6, max=40)])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=6, max=25)])
+    confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
