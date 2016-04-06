228ac15e5853f36a00fad1fdb647cfc14720371d
Jeff Reiher <jreiher2003@yahoo.com>
2016-04-06 14:40:06 -0400

    Comment out ssl again

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask 
-from flask_sslify import SSLify 
+# from flask_sslify import SSLify 
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt 
 from flask.ext.login import LoginManager 
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 # print os.environ['APP_SETTINGS']
 # print app.config['POSTS_PER_PAGE']
-sslify = SSLify(app)
+# sslify = SSLify(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
