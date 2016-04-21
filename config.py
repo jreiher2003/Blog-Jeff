2964feb7a5986a2c4e2eef56bc9cfc84f89a227c
Jeff Reiher <jreiher2003@yahoo.com>
2016-04-21 11:44:24 -0400

    Add AWS configs

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     # UPLOADS_DEFAULT_DEST = "/static/tmp/"
+    AWS_ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
+    AWS_SECRET_KEY = os.environ["AWS_SECRET_KEY"]
+    S3_BUCKET = os.environ["S3_BUCKET"]
 
 
 class TestConfig(BaseConfig):
