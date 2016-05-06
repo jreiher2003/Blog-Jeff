734c57268416e29ff7a52a1edb94335aceb6e7e4
Jeff Reiher <jreiher2003@yahoo.com>
2016-05-06 10:43:30 -0400

    Remove api key to url directly

--- app/views.py
+++ app/views.py
@@ -136,8 +136,6 @@ def delete_blogpost(author_id, blog_id):
 def projects():
     error = None
     form = LoginForm(request.form)
-    thumb_key = os.environ["THUMBALIZR_KEY"]
-    
     headers = {"Content-Type": "application/json", "User-Agent": "jreiher2003"}
     puppy = requests.get("https://api.github.com/repos/jreiher2003/Puppy-Adoption", headers=headers).json()
     portfolio = requests.get("https://api.github.com/repos/jreiher2003/Jeff-Portfolio", headers=headers).json()
@@ -150,8 +148,7 @@ def projects():
         puppy=puppy,
         portfolio=portfolio,
         wiki=wiki,
-        composite=composite,
-        thumb_key=thumb_key
+        composite=composite
         )
 
 
